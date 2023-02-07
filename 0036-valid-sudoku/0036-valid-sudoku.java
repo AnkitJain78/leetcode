5b83bc5758736aaa460135de85ca896fdd696e28
Ankit Jain <89885283+AnkitJain78@users.noreply.github.com>
2023-02-07 10:37:14 +0530

    Time: 15 ms (32.47%), Space: 42.8 MB (49.16%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> temp = new HashSet<>();
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') {
+                    int n = board[r][c] - '0';
+                    if(!temp.add(n + "row" + r) ||
+                    !temp.add(n + "col" + c) ||
+                    !temp.add(n + "grid" + r/3 + "" + c/3))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
